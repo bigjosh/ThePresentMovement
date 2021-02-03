e43928ce87032c8613645b6d065665363f19a8b5
josh <github@junk.josh.com>
2021-02-03 00:19:35 -0500

    Initial commit.

---
+++ DayMoonYearMovement/DayMoonYearMovement/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * DayMoonYearMovement.cpp
+ *
+ * Created: 2/3/2021 12:04:26 AM
+ * Author : passp
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
