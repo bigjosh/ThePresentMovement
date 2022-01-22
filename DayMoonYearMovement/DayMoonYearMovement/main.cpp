9491bdcf6201974bc77abd41b00c1cc2aa471e4f
josh <github@junk.josh.com>
2022-01-22 14:16:21 -0500

    Add hour

--- DayMoonYearMovement/DayMoonYearMovement/main.cpp
+++ DayMoonYearMovement/DayMoonYearMovement/main.cpp
@@ -459,6 +459,10 @@ void waitButtonPress() {
 #define TROPICALYEAR_SECS (31556925UL)
 #define TROPICALYEAR_SECS_PER_TICK ( SOLARDAY_SECS/ TICKS_PER_ROTATION )
 
+#define HOUR_SECS	(60*60)
+#define HOUR_SECS_PER_TICK ( HOUR_SECS / TICKS_PER_ROTATION )
+
+
 // Human life is long enough that we have to switch to minutes
 
 #define HUMANLIFE_YEARS (120)
@@ -521,11 +525,20 @@ int main(void)
 		
 	#define SECS_PER_SCOTTLIFE (1335721787UL)
 	#define MINS_PERSCOTTLIFE (SECS_PER_SCOTTLIFE/SECS_PER_MIN)
+
+/*
 	
 	// The numbers are just too big for even long ints, so use a float. 
 	const double HUMANLIFE_SECS =  (1.0 * HUMANLIFE_YEARS * TROPICALYEAR_SECS);
 	
 	spin( (unsigned) ( (SECS_PER_SCOTTLIFE / HUMANLIFE_SECS ) * 3600.0) );			
+	
+*/
+
+
+	//spin(  TICKS_PER_ROTATION / 2  );		// Make 180 deg rotation on button press to show we are working
+	spin(  100 );		// Make 180 deg rotation on button press to show we are working
+
 		
 	// *** Enable interrupt on /INT signal from RTC
 
@@ -533,12 +546,12 @@ int main(void)
 
 	
 	// *** Program RTC to generate /INT signal period
-	
-	//rx8900_fixed_timer_set_seconds( SOLARDAY_SECS_PER_TICK );		
+	rx8900_setup();	
+	rx8900_fixed_timer_set_seconds( HOUR_SECS_PER_TICK );		
+	//rx8900_fixed_timer_set_seconds( SOLARDAY_SECS_PER_TICK );	
 	//rx8900_fixed_timer_set_seconds( LUNARMONTH_SECS_PER_TICK );
-	rx8900_fixed_timer_set_minutes( HUMANLIFE_MINS_PER_TICK );
-			
-			
+	//rx8900_fixed_timer_set_minutes( HUMANLIFE_MINS_PER_TICK );
+						
 	normalStepMode();			// Start stepping, never returns
 	
 	__builtin_unreachable();
