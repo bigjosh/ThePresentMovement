d2eeee31d3f9653e1cfd281c4cdce7c5fcdd9f00
josh <github@junk.josh.com>
2023-07-15 11:55:22 -0400

    Initial startup fast ticking working, before lambda implementation

--- programming/bin/day.bat
+++ programming/bin/day.bat
@@ -1,4 +1,4 @@
-avrdude -Cavrdude.conf -v -pattiny25 -cusbtiny -Uflash:w:day.hex:i -u 
+avrdude -Cavrdude.conf -v -pattiny25 -cusbtiny -Uflash:w:day.hex:i -u -B 3
 @if NOT ["%errorlevel%"]==["0"] (
     @color 47
     @echo ERROR CLOCK NOT PROGRAMMED!
